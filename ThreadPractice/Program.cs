8b510a2b2108d22d60a315ced9f92a8ea6fd54a0
Max Wang <max.wang@beteasy.com.au>
2020-06-12 00:07:23 +1000

    add thread join

--- ThreadPractice/Program.cs
+++ ThreadPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThreadPractice
 {
@@ -7,8 +8,18 @@ namespace ThreadPractice
         static void Main(string[] args)
         {
             
-            var task1 = new MonitorTryEnter();
-            task1.DoSomething();
+            //var task1 = new MonitorTryEnter();
+            //task1.DoSomething();
+
+            var task2 = new ThreadJoin();
+            task2.DoSomething();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine($"Main Thread {i}");
+
+            }
 
             Console.ReadLine();
         }
---
+++ ThreadPractice/ThreadJoin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPractice
+{
+    public class ThreadJoin
+    {
+        //Blocks the calling thread until the thread represented by this instance terminates.
+        //Join(Int32)	
+        //Blocks the calling thread until the thread represented by this instance terminates or the specified time elapses, while continuing to perform standard COM and SendMessage pumping.
+        
+        public void DoSomething()
+        {
+            var thread1 = new Thread(StartThread1);
+            var thread2 = new Thread(StartThread2);
+
+            thread1.Start();
+            thread2.Start();
+
+            //thread1.Join();
+            //thread2.Join();
+        }
+
+
+        private void StartThread2()
+        {
+            Console.WriteLine("Thread 2 Start");
+            Thread.Sleep(2000);
+            Console.WriteLine("Thread 2 Finished");
+        }
+
+        private void StartThread1()
+        {
+            Console.WriteLine("Thread 1 Start");
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread 1 Finished");
+        }
+    }
+}
